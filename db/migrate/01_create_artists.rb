ce0d7c56f578052105208e216b0f3c905eb95fea
Matt Gordon <bigmattsize21@gmail.com>
2020-05-16 01:19:27 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,14 +1,7 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
-  
-  def self.create_table
-    sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS songs (
-      id INTEGER PRIMARY KEY, 
-      title TEXT, 
-      length INTEGER
-      )
-    SQL
-    ActiveRecord::Base.connection.execute(sql)
+  def up 
+  end
+  def down
   end
 
   def change
